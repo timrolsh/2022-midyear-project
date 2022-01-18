2562fcf3c89481fa237209a5babf29e69f67c507
joshuakim05 <joskim24@bergen.org>
2022-01-18 10:15:02 -0500

    Setup game.py

---
+++ game.py
@@ -0,0 +1,56 @@
+import pygame, sys
+from pygame.locals import *
+# from City import *
+# from Color import *
+# # from Computer import *
+# from Field import *
+# from Goal import *
+# from Human import *
+# from Player import *
+# from Track import *
+
+FPS = 15
+DISPLAYX = 1471
+DISPLAYY = 979
+
+
+pygame.init()
+screen=pygame.display.set_mode([DISPLAYX, DISPLAYY])
+pygame.display.set_caption("Ticket to Ride")
+bg = pygame.image.load("board2.jpg")
+#to do: resize
+
+
+def game_loop():
+    
+    running = True
+    while running:
+
+       
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        draw_game_area()
+        
+        pygame.display.update()
+        
+
+    pygame.quit()
+    
+def start_screen():
+    #to do
+    game_loop()
+
+def draw_game_area():
+    screen.blit(bg, (0,0))
+    #add appropriate overlays, buttons
+
+def main():
+    while True:
+        start_screen()
+        
+        
+main()
+
+

ea2190dc84363e832f148a2557e1c0dac0c181ce
RohanJoshi28 <78099546+RohanJoshi28@users.noreply.github.com>
2022-01-19 10:44:41 -0500

    resized display and images, and drew circles for the cities

--- game.py
+++ game.py
@@ -3,22 +3,28 @@ from pygame.locals import *
 # from City import *
 # from Color import *
 # # from Computer import *
-# from Field import *
+from Field import *
 # from Goal import *
 # from Human import *
 # from Player import *
 # from Track import *
 
 FPS = 15
-DISPLAYX = 1000
-DISPLAYY = 600
+DISPLAYX = 1051
+DISPLAYY = 750
 
+ORIGINALX = 2941
+ORIGINALY = 1958
+
+CITY_COLOR = (219,152,99,1)
 
 pygame.init()
 screen=pygame.display.set_mode([DISPLAYX, DISPLAYY])
 pygame.display.set_caption("Ticket to Ride")
 bg = pygame.image.load("board.jpg")
-bg = pygame.transform.scale(bg, (1000, 600))
+bg = pygame.transform.scale(bg, (DISPLAYX, DISPLAYY))
+
+
 
 def game_loop():
     
@@ -29,6 +35,10 @@ def game_loop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if screen.get_at(pygame.mouse.get_pos()) == CITY_COLOR:
+                    
+
 
         draw_game_area()
         
@@ -43,7 +53,9 @@ def start_screen():
 
 def draw_game_area():
     screen.blit(bg, (0,0))
-    #add appropriate overlays, buttons
+    for city in Field.cities.keys():
+        city = Field.cities[city]
+        pygame.draw.circle(screen, CITY_COLOR, (float(DISPLAYX/ORIGINALX) * float(city.x), float((DISPLAYY/ORIGINALY)) * float(city.y)), 10) 
 
 def main():
     while True:

efcb4705d9f2f79620da7e9f7751a877cd8b8bd5
timrolsh <68395794+timrolsh@users.noreply.github.com>
2022-01-24 15:01:53 -0500

    fixed few issues w/nico

--- Field.py
+++ Field.py
@@ -3,21 +3,22 @@ from Track import Track
 
 
 class Field:
+    PLAYER_CIRCLE_RADIUS = 20
     cities = {}
     tracks_list = []
-    city_list_file = open("data/city_list.txt")
-    tracks_list_file = open("data/tracks_list.txt")
+    city_list_txt = open("data/city_list.txt")
+    tracks_list_txt = open("data/tracks_list.txt")
 
     # go through every line in the file of cities and their corresponding coordinates on the
-    for line in city_list_file:
+    for line in city_list_txt:
         data = line.split(", ")
         cities[data[0]] = City(data[0], int(data[1]), int(data[2]))
-    city_list_file.close()
+    city_list_txt.close()
 
     # create last_track variable to store last track read from file
     last_track = None
     # go through each line in the line of tracks
-    for line in tracks_list_file:
+    for line in tracks_list_txt:
         data = line.split(": ")
         # if the line is declaring info about a specific track
         if len(data) == 1:
@@ -28,6 +29,7 @@ class Field:
         # otherwise, the line must be declaring a traincar with two given points
         else:
             coordinates = data[1].split(", ")
-            last_track.add_train_car([data[0], data[1]], [data[2], data[3]])
+            coordinates[3].rstrip("\n")
+            last_track.add_train_car([int(coordinates[0]), int(coordinates[1])], [int(coordinates[2]), int(coordinates[3])])
 
-    tracks_list_file.close()
+    tracks_list_txt.close()
--- Human.py
+++ Human.py
@@ -3,3 +3,4 @@ from Player import Player
 
 class Human(Player):
     # TODO implement methods for human player.
+    pass

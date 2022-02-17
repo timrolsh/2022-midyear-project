1cf73953fd0630dcbdb6317e3c300a425d94c03e
timrolsh <tim.rolsh@gmail.com>
2022-02-16 22:00:38 -0500

    a

---
+++ MACOSLAUNCH.sh
@@ -0,0 +1,15 @@
+#define parameters
+INTERPRETERNAME="python3"
+VENV_NAME="venv"
+
+#create a virtual environment in the directory to run our program if one does not already exist on the computer
+if [ ! -d "${VENV_NAME}" ]; then
+    ${INTERPRETERNAME} -m venv ${VENV_NAME}
+    #activate virtual envriornment
+    source ${VENV_NAME}/Scrips/activate
+    #install pygame (from requirements.txt) into the virtual enviornment
+    ${INTERPRETERNAME} -m pip install -r requirements.txt
+fi
+
+# run game launcher
+${INTERPRETERNAME} Game.p
